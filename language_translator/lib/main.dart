cbac32e4ab115cb0affb2855f395e8b0670a9b28
Pranav <pranav.bhawan@gmail.com>
2020-12-15 18:53:49 +0530

    Added Language Translator app, incomplete though

---
+++ language_translator/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:translator/translator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  Future<void> convert() async {
+    final translator = GoogleTranslator();
+    final input = "Здравствуйте. Ты в порядке?";
+
+    // Using the Future API
+    translator
+        .translate(input, to: 'en')
+        .then((result) => print("Source: $input\nTranslated: $result"));
+
+    // Passing the translation to a variable
+    var translation = await translator
+        .translate("I would buy a car, if I had money.", from: 'en', to: 'it');
+
+    // You can also call the extension method directly on the input
+    print('Translated: ${await input.translate(to: 'en')}');
+
+    // For countries that default base URL doesn't work
+    translator.baseUrl = "translate.google.cn";
+    translator.translateAndPrint("This means 'testing' in chinese", to: 'zh-cn');
+    //prints 这意味着用中文'测试'
+
+    print("translation: $translation");
+    // prints translation: Vorrei comprare una macchina, se avessi i soldi.
+  }
+  convert();
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+    }
+
+
